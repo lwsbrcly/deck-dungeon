04393dd4616f3ac3349617eadce550d53a1eb456
lwsbrcly <lwsbrcly@gmail.com>
2026-09-20 23:36:00 +0100

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -1,11 +1,16 @@
 const THEMES = {
 
     dungeon: {
-
-        id: 'dungeon',
+        
         name: 'Deck Dungeon',
-        description: 'Fight monsters, find weapons, and escape the dungeon.',
+        description: 'Monsters. Weapons. Potions. Can you escape alive?',
 
+        artwork: {
+            logo: 'assets/dungeon/logo.png',
+            card: 'assets/dungeon/card.png',
+            back: 'assets/dungeon/back.png',
+        },
+            
         colours: {
             bg: '#121214',
             cardBg: '#1e1e24',
@@ -22,36 +27,41 @@ const THEMES = {
         text: {
             gameTitle: 'DECK DUNGEON',
             location: 'Dungeon',
-            enemy: 'Monster',
-            weapon: 'Weapon',
-            healing: 'Food',
-            flee: 'Flee',
-            enter: 'Enter Dungeon'
+            monster: 'Monster',
+            weapon: 'Weaon',
+            potion: 'Food',
+            equip: 'equips',
+            discard: 'discards',
+            fight: 'uses',
+            fist: 'enters fist fight with',
+            heal: 'consumes',
+            flee: 'Fled the room',
+            enter: 'Enters Dungeon weilding'
         },
 
         cards: {
             monsters: {
                 clubs: {
-                    '2': 'Rat', '3': 'Cave Spider', '4': 'Wolf', '5': 'Goblin',
-                    '6': 'Orc', '7': 'Shaman', '8': 'Bandit', '9': 'Gladiator',
-                    '10': 'Dark Wizard', 'J': 'Minotaur', 'Q': 'Ogre', 'K': 'Giant', 'A': 'Dragon'
+                    2: 'Rat', 3: 'Cave Spider', 4: 'Wolf', 5: 'Goblin',
+                    6: 'Orc', 7: 'Shaman', 8: 'Bandit', 9: 'Gladiator',
+                    10: 'Dark Wizard', J: 'Minotaur', Q: 'Ogre', K: 'Giant', A: 'Dragon'
                 },
                 spades: {
-                    '2': 'Spooky Fog', '3': 'Slime', '4': 'Snakes', '5': 'Skeleton',
-                    '6': 'Zombie', '7': 'Ghost', '8': 'Ghoul', '9': 'Wraith',
-                    '10': 'Necromancer', 'J': 'Vampire', 'Q': 'Mummy', 'K': 'Lich King', 'A': 'Bone Dragon'
+                    2: 'Spooky Fog', 3: 'Slime', 4: 'Snakes', 5: 'Skeleton',
+                    6: 'Zombie', 7: 'Ghost', 8: 'Ghoul', 9: 'Wraith',
+                    10: 'Necromancer', J: 'Vampire', Q: 'Mummy', K: 'Lich King', A: 'Bone Dragon'
                 }
             },
             weapons: {
                 diamonds: {
-                    '2': 'Dagger', '3': 'Club', '4': 'Short Sword', '5': 'Mace',
-                    '6': 'Longsword', '7': 'Battle Axe', '8': 'Warhammer', '9': 'Great Axe', '10': 'Greatsword'
+                    2: 'Dagger', 3: 'Club', 4: 'Short Sword', 5: 'Mace',
+                    6: 'Longsword', 7: 'Battle Axe', 8: 'Warhammer', 9: 'Great Axe', 10: 'Greatsword'
                 }
             },
             potions: {
                 hearts: {
-                    '2': 'Stale Bread', '3': 'Sus Mushrooms', '4': 'Apple', '5': 'Fresh Bread',
-                    '6': 'Cooked Meats', '7': 'Roast Chicken', '8': 'Hearty Stew', '9': 'Healing Elixir', '10': 'Magic Potion'
+                    2: 'Stale Bread', 3: 'Sus Mushrooms', 4: 'Apple', 5: 'Fresh Bread',
+                    6: 'Cooked Meats', 7: 'Roast Chicken', 8: 'Hearty Stew', 9: 'Healing Elixir', 10: 'Magic Potion'
                 }
             }
         }
