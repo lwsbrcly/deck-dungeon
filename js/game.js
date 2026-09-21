a7ae7abf96f0b9006c401f9489d9728102acb042
lwsbrcly <lwsbrcly@gmail.com>
2026-09-21 15:40:27 +0100

    Match HP blocks to room counter

--- js/game.js
+++ js/game.js
@@ -1368,7 +1368,7 @@ var isSolo = state.mode !== 'coop';
   for (var i = 1; i <= state.maxHP; i++) {
   
     if (i <= p.hp) {
-      healthPath += '▓';
+      healthPath += '█';
     } else {
       healthPath += '░';
     }
