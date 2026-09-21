c9a62040edcd8e04896252683722638bf1ddfe3f
lwsbrcly <lwsbrcly@gmail.com>
2026-09-21 20:37:57 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -58,10 +58,6 @@ hearts: {
 }
 };
 
-const SUITS = {spades:'♠', clubs:'♣', diamonds:'♦', hearts:'♥'};
-const ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-function value(rank) { return rank==='A'?14:rank==='J'?11:rank==='Q'?12:rank==='K'?13:Number(rank); }
-
 // Custom SVG Dragon for Ace cards
 const DRAGON_SVG = `<svg viewBox="0 0 723 702" xmlns="http://www.w3.org/2000/svg">
 <g stroke="currentColor" stroke-width="21" stroke-linecap="round" stroke-linejoin="round" fill="none" transform="translate(-414 -1110)">
@@ -485,37 +481,6 @@ document.getElementById('overlay').classList.remove('show');
 document.getElementById('game').style.display = 'none';
 document.getElementById('setupScreen').style.display = 'block';
 showScreen('game-selector');
-}
-
-function makeDeck(excludeDagger) {
-var d = [];
-var suitKeys = Object.keys(SUITS);
-for (var i = 0; i < suitKeys.length; i++) {
-  var suit = suitKeys[i];
-  for (var j = 0; j < ranks.length; j++) {
-    var rank = ranks[j];
-    if ((suit === 'diamonds' || suit === 'hearts') && ['A','J','Q','K'].indexOf(rank) !== -1) continue;
-    if (excludeDagger && suit === 'diamonds' && rank === '2') continue;
-    d.push({
-      suit: suit,
-      rank: rank,
-      value: value(rank),
-      name: CARD_NAMES[suit][rank],
-      id: suit + rank
-    });
-  }
-}
-return shuffle(d);
-}
-
-function shuffle(a) {
-for (var i = a.length - 1; i > 0; i--) {
-  var j = Math.floor(Math.random() * (i + 1));
-  var temp = a[i];
-  a[i] = a[j];
-  a[j] = temp;
-}
-return a;
 }
 
 function startGame() {
