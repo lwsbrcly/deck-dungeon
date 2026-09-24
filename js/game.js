c3c53b796349c67637d9b9c2c41d6b7340d191f2
lwsbrcly <lwsbrcly@gmail.com>
2026-09-24 16:00:57 +0100

    Show PNG weapon artwork directly

--- js/game.js
+++ js/game.js
@@ -390,7 +390,7 @@ function cardHTML(c, customCornerText) {
       : null;
     
     if (pngArt) {
-      centerArt = '<div class="card-art png-art" style="--card-art-image: url(\'' + pngArt + '\');"></div>';
+      centerArt = '<div class="card-art png-art"><img src="' + pngArt + '" alt=""></div>';
     } else if (svgArt) {
       centerArt = '<div class="card-art">' + svgArt + '</div>';
     } else {
